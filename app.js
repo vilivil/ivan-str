127d4800f1f925bebb115fab2cb5640f74cb39f7
vilivil <54793091+vilivil@users.noreply.github.com>
2020-04-14 22:03:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = '<I love tech and exploring new things.Bye./>';
+var speed = 60;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
